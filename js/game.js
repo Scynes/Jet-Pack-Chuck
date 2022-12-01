3497d2471ec2b2b96e33b0439e6db26bdbe4a3d9
Scynes <dustin.greyfield@gmail.com>
2022-12-01 14:58:33 -0500

    environment building and reference binding

--- js/game.js
+++ js/game.js
@@ -34,24 +34,87 @@ const $gameContext = $gameCanvas.get(0).getContext('2d');
 let spriteInfo;
 
 /**
- * The frame state names. Also references the key for values within the spriteInfo object.
+ * The current game frames.
  */
-const animationFrameStates = ['chuckIdle', 'chuckIgnition', 'chuckBlasting', 'chuckIdle'];
+let gameFrames = 0;
+
+const buildSpriteReferences = () => {
+    game.ready.gr = spriteInfo.gameReady;
+    environment.background.bg = spriteInfo.gameBackground;
+    environment.foreground.fg = spriteInfo.gameForeground;
+}
 
 /**
  * Holds animation frame related information.
  */
 const chuckAnimation = {
 
-    frame: [
+    frames: [
         {state: 'chuckIdle', x: 0, y: 0, width: 0, height: 0}, //1st frame
         {state: 'chuckIgnition', x: 0, y: 0, width: 0, height: 0}, //2nd frame
         {state: 'chuckBlasting', x: 0, y: 0, width: 0, height: 0}, //3rd frame
         {state: 'chuckIgnition', x: 0, y: 0, width: 0, height: 0}  //4th frame
     ],
+    frame: 2,
     canvasX: 50,
-    canvasY: 150
+    canvasY: 150,
 
+    // Draws the animation based on the current frame.
+    create: function () {
+        var chuck = this.frames[this.frame];
+
+        $gameContext.drawImage(sprite, chuck.x, chuck.y, chuck.width, chuck.height, this.canvasX, this.canvasY, chuck.width, chuck.height);
+    }
+
+};
+
+const game = {
+
+    ready: {
+        canvasX: 0,
+        canvasY: 228,
+        gr: undefined,
+
+        create: function() {
+            //$gameContext(sprite, )
+        }
+    },
+    over: {
+
+    }
+};
+
+/**
+ * Holds game environmental detail information.
+ */
+const environment = {
+
+    background: {
+        canvasX: 0,
+        canvasY: $gameCanvas.height() - 204,
+        bg: undefined,
+
+        // Draws the background image to the canvas.
+        create: function() {
+
+            $gameContext.drawImage(sprite, this.bg.x, this.bg.y, this.bg.width, this.bg.height, this.canvasX, this.canvasY, this.bg.width, this.bg.height);
+            $gameContext.drawImage(sprite, this.bg.x, this.bg.y, this.bg.width, this.bg.height, this.canvasX + this.bg.width, this.canvasY, this.bg.width, this.bg.height);
+
+        }
+    },
+    foreground: {
+        canvasX: 0,
+        canvasY: $gameCanvas.height() - 112,
+        fg: undefined,
+
+        // Draws the foreground image to the canvas.
+        create: function() {
+
+            $gameContext.drawImage(sprite, this.fg.x, this.fg.y, this.fg.width, this.fg.height, this.canvasX, this.canvasY, this.fg.width, this.fg.height);
+            $gameContext.drawImage(sprite, this.fg.x, this.fg.y, this.fg.width, this.fg.height, this.canvasX + this.fg.width, this.canvasY, this.fg.width, this.fg.height);
+
+        }
+    }
 };
 
 /**
@@ -66,7 +129,7 @@ const loadJSON = () => {
             spriteInfo = data;
             
             // Populates the animation frame data.
-            chuckAnimation.frame.forEach(element => {
+            chuckAnimation.frames.forEach(element => {
                 element.x = spriteInfo[element.state].x;
                 element.y = spriteInfo[element.state].y;
                 element.width = spriteInfo[element.state].width;
@@ -74,7 +137,6 @@ const loadJSON = () => {
             });
         }
     })
-    console.log(chuckAnimation.frame[2])
 };
 
 /**
@@ -85,6 +147,11 @@ const paint = () => {
 
     $gameContext.fillStyle = '#70c5ce';
     $gameContext.fillRect(0, 0, $gameCanvas.width(), $gameCanvas.height());
+
+    environment.background.create();
+    environment.foreground.create();
+    chuckAnimation.create();
+
 };
 
 /**
@@ -95,15 +162,20 @@ const tick = () => {
     paint();
 
     //Perform animation request.
-    requestAnimationFrame(tick);
+    //requestAnimationFrame(tick);
 
 };
 
 sprite.onload = () => {
-    loadJSON();
-    var idleChuck = spriteInfo.gameCactiDown;
-    $gameContext.drawImage(sprite, idleChuck.x, idleChuck.y, idleChuck.width, idleChuck.height, 110, 0, idleChuck.width, idleChuck.height)
 
+    // Load the JSON sprite data.
+    loadJSON();
+
+    // Assigns JSON sprite references.
+    buildSpriteReferences();
+
+    // Begin game logic ticking.
+     tick();
 }
 
 
